f24e93adbeb27f1615e257f6f444773eeca19fb7
upleung <georglee9830@gmail.com>
2026-09-09 11:06:55 +0800

    up

--- src/core/ping.js
+++ src/core/ping.js
@@ -21,7 +21,7 @@ function buildStealthHeaders(ua) {
   const headers = {
     "User-Agent": ua,
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Accept-Language": "en-US,en;q=0.9",
     "Accept-Encoding": "gzip, deflate, br",
     "Cache-Control": "max-age=0",
     "Upgrade-Insecure-Requests": "1",
